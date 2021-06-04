5186343c9b026efca9a43159cdb53863558656ee
CharlotteEssajee <71342246+CharlotteEssajee@users.noreply.github.com>
2021-06-04 19:29:09 +0200

    minor changes
    and adding a success message to the contact form

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,4 +1,5 @@
 // contact form
+const fullContactForm = document.querySelector(".contact");
 const form = document.querySelector("#contactForm");
 const fullname = document.querySelector("#fullName");
 const fullNameError = document.querySelector("#fullNameError");
@@ -12,28 +13,42 @@ const messageError = document.querySelector("#messageError");
 function validateForm(event) {
   event.preventDefault();
 
+  let form = true;
+
   if (checkLength(fullname.value, 4) === true) {
     fullNameError.style.display = "none";
   } else {
     fullNameError.style.display = "block";
+    form = false;
   }
 
   if (validateEmail(email.value) === true) {
     emailError.style.display = "none";
   } else {
     emailError.style.display = "block";
+    form = false;
   }
 
   if (checkLength(subject.value, 15) === true) {
     subjectError.style.display = "none";
   } else {
     subjectError.style.display = "block";
+    form = false;
   }
 
   if (checkLength(message.value, 24) === true) {
     messageError.style.display = "none";
   } else {
     messageError.style.display = "block";
+    form = false;
+  }
+
+  if (form === true) {
+    fullContactForm.innerHTML = `<div class="messageSuccess">
+                                  <i class="far fa-check-circle"></i>
+                                  <p class="success">Thank you!</p>
+                                  <p>Your message has been sent</p>
+                                </div>`;
   }
 }
 
